3eb076e09bdb350327e0288b8a66e2c78c0d26c5
ShreyaKumbhar05 <kumbharshreya016@gmail.com>
2026-08-03 20:18:25 +0530

    Add files via upload

---
+++ findChar.java
@@ -0,0 +1,13 @@
+// Find the character at given index.
+
+class findChar
+{
+	public static void main(String [] args)
+	{
+		String s = "morning";
+		int n=5;
+
+		int ind = n- 1;
+		System.out.println(s.charAt(ind));
+	}
+}
