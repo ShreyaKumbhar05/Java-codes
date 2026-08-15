358a86d6dcbadceef85d56bc324509db5d19ff99
ShreyaKumbhar05 <kumbharshreya016@gmail.com>
2026-08-15 22:34:26 +0530

    Add files via upload

---
+++ stack.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+class parentheses
+{
+	public static void main(String [] args)
+	{
+		char [] arr = {'(',')','[',']','{','}'};
+		Stack<Character> st = new Stack<>();
+
+		boolean valid = true;
+		for(int i=0; i<arr.length; i++)
+		{
+			if(arr[i] == '(')
+			st.push(')');
+
+			else if(arr[i] == '[')
+			st.push(']');
+
+			else if(arr[i] == '{')
+			st.push('}');
+			
+			else if(st.pop() != arr[i] || !st.isEmpty())
+			{
+				valid = false;
+			}
+		}
+		if(valid)
+		{
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+
+		}				
+	}
+}
