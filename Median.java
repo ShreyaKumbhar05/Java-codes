9238250a191af57fc16923873aa980d3e81dd41e
ShreyaKumbhar05 <kumbharshreya016@gmail.com>
2026-08-29 10:38:50 +0530

    Add files via upload

---
+++ Median.java
@@ -0,0 +1,12 @@
+// find the median of an array.
+
+class Median {
+    public static void main(String[] args) {
+        int n = 5; 
+        int array[] = {2,4,5,6,8};
+
+        int a = array.length/2;
+
+        System.out.println(array[a]);
+    }
+}
